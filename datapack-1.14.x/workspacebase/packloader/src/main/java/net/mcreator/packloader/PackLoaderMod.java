bc26e5554b6601806e88a48a78d05bb556f3b678
Spectrall368 <105946758+Spectrall368@users.noreply.github.com>
2025-08-18 18:31:32 +0200

    Fix PackLoader

--- datapack-1.14.x/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
+++ datapack-1.14.x/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
@@ -1,24 +1,33 @@
 package net.mcreator.packloader;
 
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.event.AddPackFindersEvent;
 
-import net.minecraft.server.packs.repository.PackSource;
-import net.minecraft.server.packs.repository.FolderRepositorySource;
+import net.minecraft.resources.ResourcePackInfo;
+import net.minecraft.resources.IPackFinder;
+import net.minecraft.resources.FolderPack;
+import net.minecraft.client.Minecraft;
 
+import java.util.Map;
 import java.io.File;
 
-@Mod(PackLoaderMod.MODID) public class PackLoaderMod {
+@Mod(PackLoaderMod.MODID)
+public class PackLoaderMod {
+    public static final String MODID = "packloader";
 
-	public static final String MODID = "packloader";
+    public PackLoaderMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::addPacks);
+    }
 
-	public PackLoaderMod() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::addPacks);
-	}
-
-	public void addPacks(AddPackFindersEvent event) {
-		event.addRepositorySource(new FolderRepositorySource(new File("datapacks"), PackSource.DEFAULT));
-	}
-
+    public void addPacks(FMLClientSetupEvent event) {
+        Minecraft mc = event.getMinecraftSupplier().get();
+        mc.getResourcePackRepository().addSource(new IPackFinder() {
+            @Override
+            public <T extends ResourcePackInfo> void loadPacks(Map<String, T> consumer, ResourcePackInfo.IFactory<T> factory) {
+                if (mc.player != null)
+                    consumer.put("packloader", ResourcePackInfo.create("packloader", true, () -> new FolderPack(new File("datapacks")), factory, ResourcePackInfo.Priority.TOP));
+            }
+        });
+    }
 }
